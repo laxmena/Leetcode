219ab432401cce2caaafd41a8a8efcfb0212581d
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-01-24 07:57:17 -0600

    Time: 20 ms (5.36%), Space: 10.6 MB (34.95%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         map<int, int> freq, res;
+        vector<int> result;
+
         for(auto n: nums1) {
             if(freq.find(n) == freq.end()) freq[n] = 0;
             freq[n]++;
         }
         for(auto n: nums2) {
             if(freq.find(n) != freq.end()) {
-                if(res.find(n) == res.end()) res[n] = 0;
-                res[n]++; freq[n]--;
+                result.push_back(n);
+                freq[n]--;
                 if(freq[n] == 0) freq.erase(n);
             }
         }
-        vector<int> result;
-        for(auto& [key, value]: res) {
-            for(int i=0; i<value; i++) result.push_back(key);
-        }
+
         return result;
     }
 };
