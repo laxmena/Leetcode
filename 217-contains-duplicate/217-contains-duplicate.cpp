2c0c030ee284fcb1472a7872c1d2c27114fe51ad
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-01-24 07:13:56 -0600

    Time: 104 ms (47.57%), Space: 52.2 MB (19.68%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> mem;
+        for(auto n: nums) {
+            if(mem.find(n) != mem.end()) return true;
+            mem.insert(n);
+        }
+        return false;
+    }
+};
