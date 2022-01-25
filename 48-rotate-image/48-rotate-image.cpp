96fc775f7b33b31f532c3ed1858c8ef0b70c944e
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-01-25 08:02:52 -0600

    Time: 4 ms (51.35%), Space: 7 MB (92.97%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& m) {
-        // Two Step Rotation
         // Step 1: Rotate on Axis
-        for(int i=0; i<m.size(); i++) for(int j=0; j<=i; j++) swap(m[i][j], m[j][i]);
+        for(int i=0; i<m.size(); i++) 
+            for(int j=0; j<=i; j++) 
+                swap(m[i][j], m[j][i]);
 
         // Flip
         int half = m.size() / 2;
-        for(int i=0; i<m.size(); i++) {
-            for(int j=0; j<half; j++) {
+        for(int i=0; i<m.size(); i++)
+            for(int j=0; j<half; j++)
                 swap(m[i][j], m[i][m.size()-j-1]);
-            }
-        }
     }
 };
