fc6955ba7ca2954510aab71ce10573074530717b
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-01-27 17:50:09 -0600

    Time: 0 ms (100.00%), Space: 7 MB (82.95%) - LeetHub

---
+++ 163-missing-ranges/163-missing-ranges.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string get_range(int start, int end)
+    {
+        return start==end? to_string(start) : to_string(start)+"->"+to_string(end);
+    }
+    vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        vector<string> result;
+        int pre = lower-1;
+        for(int i =0; i <= nums.size(); i++)
+        {
+           int cur = (i==nums.size()? upper+1:nums[i]);
+           if(cur-pre>=2)
+            result.push_back(get_range(pre+1,cur-1));
+            pre = cur;
+        }
+        return result;
+    }
+};
