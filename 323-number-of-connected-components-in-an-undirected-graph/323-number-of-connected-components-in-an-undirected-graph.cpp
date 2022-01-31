957e93f2397a51ed2218e9679130b5f1fd448294
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-01-31 07:27:51 -0600

    Time: 83 ms (5.20%), Space: 23 MB (5.20%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void recurse(int edge, map<int, set<int> >& mem, set<int> &isVisited) {
+        // Return if already visited
+        if(isVisited.find(edge) != isVisited.end()) return;
+        // Add edge to visited list
+        isVisited.insert(edge);
+        // If not visited, get the connected elements
+        set<int> connections = mem[edge];
+
+        for(auto c: connections) {
+            if(isVisited.find(c) != isVisited.end()) continue;
+            recurse(c, mem, isVisited);
+            isVisited.insert(c);
+            mem.erase(c);
+        }
+    }
+    int countComponents(int n, vector<vector<int>>& edges) {
+        map<int, set<int> > mem;
+        set<int> isVisited;
+        for(auto edge: edges) {
+            int e1 = edge[0], e2 = edge[1];
+            if(mem.find(e1) == mem.end()) mem[e1] = set<int>();
+            if(mem.find(e2) == mem.end()) mem[e2] = set<int>();
+            mem[e1].insert(e2);
+            mem[e2].insert(e1);
+        }
+        
+        for(int i=0; i<n; i++) {
+            recurse(i, mem, isVisited);
+        }
+        for(auto &[key, val]: mem) {
+            cout << key << " ";
+        }
+        return mem.size();
+    }
+};
