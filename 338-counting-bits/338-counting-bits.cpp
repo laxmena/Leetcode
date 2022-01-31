c74c28aa5ee5236dea987059d61ce2f700cd359f
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-01-31 06:47:18 -0600

    Time: 4 ms (89.28%), Space: 7.9 MB (74.52%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> bits(n+1, 0);
-        for(int i=0; i<=n; i++) {
-            int num = i;
-            int b = 0;
-            while(num) { b+=(num&1); num >>= 1;}
-            bits[i] = b;
+        for(int x=1; x<=n; x++) {
+            bits[x] = bits[x>>1] + (x&1);
         }
         return bits;
     }
