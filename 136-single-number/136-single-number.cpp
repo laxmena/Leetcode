6d526f83b6b004c0ab22f977566946c6282d3075
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-01-31 14:48:11 -0600

    Time: 12 ms (98.01%), Space: 17 MB (50.31%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int x = 0;
-        for(auto n: nums) x = x^n; 
+        for(auto n: nums) x ^= n;
         return x;
     }
 };
