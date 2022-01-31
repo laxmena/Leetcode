88036b4c29d06c05deecc8341979bdd8795294b4
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-01-30 21:34:17 -0600

    Time: 7 ms (10.16%), Space: 5.9 MB (74.71%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int sum = 0;
-        while(num) {
-            sum += num%10;
-            num /= 10;
-            if(num == 0 && sum > 9) {
-                num = sum;
-                sum = 0;
-            }
-        }
-        return sum;
+        if (num == 0) return 0;
+        if(num % 9 == 0) return 9;
+        return num % 9;
     }
 };
