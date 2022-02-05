b2541214bdb023bfd4bfe2fd1f12313d4b21d4dc
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-05 05:34:12 -0600

    Time: 57 ms (14.79%), Space: 14.6 MB (51.80%) - LeetHub

---
+++ 929-unique-email-addresses/929-unique-email-addresses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        set<string> unique;
+        for(string email: emails) {
+            string em = "";
+            bool at = false;
+            for(int i=0; i<email.length(); i++) {
+                char c = email[i];
+                if(c == '+') {
+                    while(email[i] != '@' && i<email.length()) { i++; }
+                    if(!at) at = true;
+                    em += email[i];
+                    continue;
+                }
+                if(c == '@') at = true;
+                if(c == '.' && !at) continue;
+                em += c;
+            }
+            cout << em << " ";
+            unique.insert(em);
+        }
+        return unique.size();
+    }
+};
