189fde90b87acfdc19f2eec47d1aed7a42717982
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-08 16:10:32 -0600

    Time: 326 ms (41.55%), Space: 74.2 MB (14.31%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> result(n+1, 0);
+        for(auto book: bookings) {
+            result[book[0]-1] += book[2];
+            result[book[1]] -= book[2];
+        }
+        for(int i=1; i<n+1; i++) {
+            result[i] += result[i-1];
+        }
+        result.erase(result.begin()+n);
+        return result;
+    }
+};
