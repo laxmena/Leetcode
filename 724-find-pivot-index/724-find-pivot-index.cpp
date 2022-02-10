91ffd4e8f70be007179692035b2324833a68eb9b
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-10 13:25:24 -0600

    Time: 28 ms (64.41%), Space: 31.2 MB (17.95%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -3,10 +3,10 @@ public:
     int pivotIndex(vector<int>& nums) {
         int totalSum = 0, leftSum=0;
         for(int n: nums) totalSum += n;
-        for(int i=0; i<nums.size(); i++) {
-            totalSum -= nums[i];
-            leftSum = leftSum + (i>0 ? nums[i-1]: 0);
-            if(totalSum == leftSum) return i;
+        for(int i=0; i<nums.size(); ++i) {
+            if(leftSum == totalSum - leftSum - nums[i]) return i;
+
+            leftSum += nums[i];
         }
         return -1;
     }
