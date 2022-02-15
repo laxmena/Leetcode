2b61f764b0eadec88d26d5ffcec19be83d9b7863
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-15 16:14:01 -0600

    Time: 18 ms (25.21%), Space: 9.7 MB (61.68%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int minCost(vector<vector<int>>& costs) {
-        vector<int> dp = costs[0];
         
         for(int i=1; i<costs.size(); i++) {
-            vector<int> cost = costs[i];
-            cost[0] += min(dp[1], dp[2]);
-            cost[1] += min(dp[0], dp[2]);
-            cost[2] += min(dp[0], dp[1]);
-            dp = cost;
+            costs[i][0] += min(costs[i-1][1], costs[i-1][2]);
+            costs[i][1] += min(costs[i-1][0], costs[i-1][2]);
+            costs[i][2] += min(costs[i-1][0], costs[i-1][1]);
         }
-
-        return min(dp[0], min(dp[1], dp[2]));
+        int n = costs.size()-1;
+        return min(costs[n][0], min(costs[n][1], costs[n][2]));
     }
 };
  
