2adb7c3d34b1812ccb940e14c5baa5373350e635
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-16 08:45:54 -0600

    Time: 82 ms (74.81%), Space: 17.3 MB (27.01%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -5,12 +5,10 @@ class Solution:
         for time in timePoints:
             hr, m = time.split(':')
             hr, m = int(hr)*60, int(m)
-            # if(hr == 0):
-            #     timearr.append(1440+m)
-                
+
             timearr.append(hr+m)
         timearr = sorted(timearr)
-        timearr += timearr
+        timearr.append(timearr[0])
         for i in range(0, len(timearr)-1):
             minVal = min(minVal, (timearr[i+1]-timearr[i])%1440)
         return minVal
