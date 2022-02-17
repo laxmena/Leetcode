abed1d2df064e9f8f64f4bac019087feca9eb32e
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-16 19:14:59 -0600

    Time: 8 ms (53.73%), Space: 9.6 MB (15.54%) - LeetHub

--- 809-expressive-words/809-expressive-words.cpp
+++ 809-expressive-words/809-expressive-words.cpp
@@ -16,11 +16,6 @@ public:
         }
         result.push_back(make_pair(prev, count));
         
-        // print vector
-        // for(auto vec: result) {
-        //     cout << vec.first << " " << vec.second << " | ";
-        // }
-        // cout << "\n";
         return result;
     }
     int expressiveWords(string s, vector<string>& words) {
@@ -28,18 +23,14 @@ public:
         int result = 0;
         for(string word: words) {
             vp dist = string_to_pair(word);
-            if(s.size() < word.size()) continue;
             if(target.size() != dist.size()) continue;
-                        
             bool flag = true; 
             for(int i=0; i<target.size() && flag; i++) {
                 flag = (target[i].first == dist[i].first);
                 if(target[i].second != dist[i].second) {
-                    // cout << target[i].first << " - " << target[i].second << " - " << dist[i].second << "\n";
                     flag = (target[i].second > 2 && target[i].second >= dist[i].second);
                 }
             }
-            // cout << word << " " << (flag?"true":"false") << "\n";
             if(flag) result++;
         }
         return result;
