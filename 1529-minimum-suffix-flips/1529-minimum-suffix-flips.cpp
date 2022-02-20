53a6d2259aad185485897f2ee36c69ded2ecfaae
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-20 17:30:56 -0600

    Time: 19 ms (53.31%), Space: 9.4 MB (85.37%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int minFlips(string target) {
-        int n = target.size();
-        int ones = 0, onezeros = 0;
-        ones += target[n-1]-'0';
-        for(int i=0; i<target.size()-1; i++) {
-            ones += target[i]-'0';
-            if(target[i] == '1' && target[i+1] == '0') onezeros+=1;
-        }
-        if(ones == 0) return 0;
-        return onezeros*2 + target[n-1]-'0';
+        int cnt = 0, state = 0;
+        for (auto b : target)
+            if (b - '0' != state) {
+                state = b - '0';
+                ++cnt;
+            }
+        return cnt;
     }
 };
