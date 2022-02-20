dae487fb8d8263db61b9340dcd08c51d26a87645
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-20 17:16:39 -0600

    Time: 0 ms (100.00%), Space: 6.2 MB (43.45%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -1,14 +1,51 @@
 class Solution {
 public:
-    string longestDiverseString(int a, int b, int c, char aa = 'a', char bb = 'b', char cc = 'c') {
-        if (a < b)
-            return longestDiverseString(b, a, c, bb, aa, cc);
-        if (b < c)
-            return longestDiverseString(a, c, b, aa, cc, bb);
-        if (b == 0)
-            return string(min(2, a), aa);
-        auto use_a = min(2, a), use_b = a - use_a >= b ? 1 : 0; 
-        return string(use_a, aa) +  string(use_b, bb) + 
-            longestDiverseString(a - use_a, b - use_b, c, aa, bb, cc);
+    string longestDiverseString(int a, int b, int c) {
+        //using max heap
+        priority_queue<pair<int,char>>pq;
+        if(a)
+        pq.push({a,'a'});
+        if(b)
+        pq.push({b,'b'});
+        if(c)
+        pq.push({c,'c'});
+        string ans="";
+        while(pq.size()>1){
+            pair<int,char>one = pq.top();pq.pop();
+            pair<int,char>two = pq.top();pq.pop();
+            if(one.first>=2){
+                ans+=one.second;
+                ans+=one.second;
+                one.first-=2;
+            }
+            else{
+                ans+=one.second;
+                one.first-=1;
+            }
+            if(two.first>=2 && two.first>=one.first){
+                ans+=two.second;
+                ans+=two.second;
+                two.first-=2;
+            }
+            else{
+                ans+=two.second;
+                two.first-=1;
+            }
+            if(one.first>0)
+                pq.push(one);
+            if(two.first>0)
+                pq.push(two);
+        }
+        if(pq.empty())
+            return ans;
+        if(pq.top().first>=2){
+            ans+=pq.top().second;
+            ans+=pq.top().second;
+        }
+        else{
+            ans+=pq.top().second;
+        }
+        return ans;
+        
     }
 };
