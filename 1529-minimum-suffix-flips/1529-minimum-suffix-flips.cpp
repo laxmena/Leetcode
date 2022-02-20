67f5745eaefaef8c046506a90643fffe6872c1b5
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-20 17:31:39 -0600

    Time: 16 ms (81.16%), Space: 9.3 MB (85.37%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int minFlips(string target) {
-        int cnt = 0, state = 0;
+        int cnt = 0;
+        char state = '0';
         for (auto b : target)
-            if (b - '0' != state) {
-                state = b - '0';
+            if (b != state) {
+                state = b;
                 ++cnt;
             }
         return cnt;
