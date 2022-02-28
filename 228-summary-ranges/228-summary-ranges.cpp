33b32d77e6a743477d36d4d70a73d4874c6d36a9
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-02-27 18:55:55 -0600

    Time: 5 ms (13.39%), Space: 6.9 MB (62.02%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> result;
+        if(nums.size() == 0) return result;
+        int start=nums[0], end;
+        for(int i=1; i<nums.size()+1; i++) {
+            if(i == nums.size() || nums[i]-1 != nums[i-1]) {
+                if(start == nums[i-1]) result.push_back(to_string(nums[i-1]));
+                else result.push_back(to_string(start) + "->" + to_string(nums[i-1]));
+                if(i < nums.size()) start = nums[i];
+            }
+        }
+        return result;
+    }
+};
