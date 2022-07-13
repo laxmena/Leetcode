7c14b962764cd7b907ed51ae897ee7cbd680776e
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-12 20:26:11 -0500

    Time: 5 ms (25.32%), Space: 6 MB (47.68%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int result = 0;
-        while(n) { result += (n&1); n>>=1; }
+        while(n) { 
+            result += (n&1); 
+            n>>=1; 
+        }
         return result;
     }
 };
