26171af24f48f3afa8478f5c21f79d1bf60fe4af
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-14 19:35:06 -0500

    Time: 25 ms (70.89%), Space: 23.1 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -4,7 +4,6 @@ public:
     int findArea(vector<vector<int>>& grid, int row, int col) {
         if(grid[row][col] != 1) return 0;
         
-        cout << row << " " << col << "\n";
         grid[row][col] = -1;
         
         int top=0, left=0, right=0, bottom=0;
@@ -22,9 +21,7 @@ public:
         
         for(int row=0; row<grid.size(); row++) {
             for(int col=0; col<grid[0].size(); col++) {
-                cout << "-->" << row << " " << col << "\n";
                 maxArea = max(findArea(grid, row, col), maxArea);
-                cout << "Max Area: " << maxArea << "\n";
             }
         }
         
