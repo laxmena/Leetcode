af34625dd8db6c8eb76e9558544c76b53222d80b
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-17 22:08:42 -0500

    Time: 207 ms (91.27%), Space: 80.8 MB (70.98%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> score(n);
+        score[0] = nums[0];
+        deque<int> dq;
+        dq.push_back(0);
+        for (int i = 1; i < n; i++) {
+            // pop the old index
+            while (!dq.empty() && dq.front() < i - k) {
+                dq.pop_front();
+            }
+            score[i] = score[dq.front()] + nums[i];
+            // pop the smaller value
+            while (!dq.empty() && score[i] >= score[dq.back()]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        return score[n - 1];
+    }
+};
