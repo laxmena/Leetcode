14924dab39ed6586704e9e34b54554b46af80864
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-18 14:38:24 -0500

    Time: 281 ms (89.93%), Space: 65.7 MB (96.08%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        vector<deque<int>> result;
-        int rows=nums.size();
-        for(int r=0; r<rows; r++) {
-            for(int c=0; c<nums[r].size(); c++) {
-                
-                while(result.size() <= (r+c)) {
-                    deque<int> temp;
-                    result.push_back(temp);
-                }
-                
-                result[r+c].push_front(nums[r][c]);
+        if (nums.empty() || nums[0].empty()) return {};
+        vector<int> res;
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        while (!q.empty()) {
+            auto [x,y] = q.front();
+            res.push_back(nums[x][y]);
+            if (x+1 < nums.size() && y == 0 ){ // node (x,y) is at the first column, add the new node below it.
+                q.push({x+1,y});
             }
+            if ( y+1 < nums[x].size() ){ // add the right neighbor node
+                q.push({x,y+1});
+            }
+            q.pop();
         }
+        return res;
         
-        vector<int> answer;
-        for(auto vec: result) {
-            for(auto val: vec) {
-                answer.push_back(val);
-            }
-        }
-        return answer;
     }
 };
