04918dac9fecff3131e13171995dcc6329a797d9
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-17 22:49:13 -0500

    Time: 2044 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -3,9 +3,6 @@ BEGIN
     SET N = N-1;
   RETURN (
       # Write your MySQL query statement below.
-      SELECT DISTINCT Salary
-         FROM Employee
-        ORDER BY Salary DESC
-        LIMIT 1 OFFSET N
+      SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET N
   );
 END
