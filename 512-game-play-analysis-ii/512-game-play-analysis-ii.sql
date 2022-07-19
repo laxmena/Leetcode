1cd87449d3d0fe98c52ec3b2fd815f97391dfe24
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-18 23:07:22 -0500

    Time: 536 ms (74.84%), Space: 0B (100.00%) - LeetHub

--- 512-game-play-analysis-ii/512-game-play-analysis-ii.sql
+++ 512-game-play-analysis-ii/512-game-play-analysis-ii.sql
@@ -1,4 +1,2 @@
-# Write your MySQL query statement below
-select player_id, device_id
-from Activity 
-where (player_id, event_date) in (select player_id, min(event_date) from Activity group by player_id)
+select distinct player_id, first_value(device_id) over (partition by player_id order by event_date) device_id
+from activity
