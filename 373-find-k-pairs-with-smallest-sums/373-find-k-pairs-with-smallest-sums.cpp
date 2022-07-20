25472a94a4ebb0e23d5502ef49b4d693549426d4
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-19 22:39:34 -0500

    Time: 368 ms (80.58%), Space: 119.3 MB (33.21%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) { // time complexity is O(m*nlogk), m = nums1 size, n = nums2 size
-        priority_queue<pair<int,pair<int,int>>> pq;  // default: the greatest comes top
+    typedef pair<int, pair<int, int>> ppii;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>> result;
+        priority_queue<ppii,vector<ppii>, greater<ppii>> pq;
+		//sum , indices
         for(int i=0;i<nums1.size();i++) {
-            for(int j=0;j<nums2.size();j++) {
-                int sum=nums1[i]+nums2[j];
-                if (pq.size()<k) {
-                    pq.push({sum,{nums1[i],nums2[j]}});
-                }
-                else if (sum<pq.top().first) {
-                    pq.pop();
-                    pq.push({sum,{nums1[i],nums2[j]}});
-                } else {
-                    break;  // save time! since we don't need to traverse the rest of vector 2
-                }
-            }
-        }
-        vector<vector<int>> ans;
-        while(!pq.empty()) {
-            ans.push_back({pq.top().second.first, pq.top().second.second});
+            pq.push({nums1[i]+nums2[0], {i,0}});
+        }        
+        while(!pq.empty() && k--){
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
             pq.pop();
+            result.push_back({nums1[i],nums2[j]});
+            if(j+1==nums2.size())
+                continue;
+            pq.push({nums1[i]+nums2[j+1] , {i,j+1}});
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return result;     
     }
 };
