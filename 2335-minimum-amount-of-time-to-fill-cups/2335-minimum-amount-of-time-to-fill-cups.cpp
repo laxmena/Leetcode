2bbe883353c21c5e7852109618de38b2b79f2177
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-24 16:45:21 -0500

    Time: 0 ms (100.00%), Space: 11.7 MB (53.28%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-    int fillCups(vector<int>& amount) {
-        priority_queue<int> rem;
-        int count = 0;
-        for(auto i: amount) {
-            if(i != 0) rem.push(i);
+    int fillCups(vector<int>& A) {
+        int mx = 0, sum = 0;
+        for(int& a: A) {
+            mx = max(a, mx);
+            sum += a;
         }
-        while(rem.size() > 1) {
-            count++;
-            int val1 = rem.top()-1;
-            rem.pop();
-            int val2 = rem.top()-1;
-            rem.pop();
-            if(val1 > 0) rem.push(val1);
-            if(val2 > 0) rem.push(val2);
-        }
-        if(rem.size() == 1) count += rem.top();
-        return count;
+        return max(mx, (sum + 1) / 2);
     }
 };
