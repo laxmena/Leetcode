2264c6449a4da53c892540b7e4ee1a86bdf92fc4
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-26 14:35:53 -0500

    Time: 322 ms (58.17%), Space: 104.6 MB (18.94%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-    int reduce(int val) {
-        if(val == 1) return -1;
-        if(val%3 == 0) return val/3;
-        if(val%2 == 1) {
-            return 1 + reduce(val-3);
-        }
-        return 1 + reduce(val-2);
-    }
-    
     int minimumRounds(vector<int>& tasks) {
         map<int, int> freq;
         int result = 0;
         for(int task: tasks) freq[task]++;        
         for(auto each: freq) {
-            if(each.second == 1) return -1; 
-            result += reduce(each.second);
+            if(each.second == 1) return -1;
+            result += (each.second+2)/3;
         }
         return result;
     }
