5b9a0acc5c368939a538cefc9fdc947fba1bcb93
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-26 16:36:25 -0500

    Time: 508 ms (98.12%), Space: 98.9 MB (86.01%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
         priority_queue<int> heap(begin(piles), end(piles));
-        int sum = 0;
-        // accumulate(begin(piles), end(piles), sum);
+        int sum = accumulate(begin(piles), end(piles), 0);
         while(k--) {
             int top = heap.top();
             heap.pop();
-            // sum -= floor(top/2);
+            sum -= floor(top/2);
             top -= floor(top/2);
             heap.push(top);
         }
-        int result = 0;
-        while(heap.empty() == false) {
-            result += heap.top();
-            heap.pop();
-        }
-        return result;
+        return sum;
     }
 };
