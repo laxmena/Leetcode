1cdabf49bd1b60731cee236408d703d741d8c3da
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-26 14:36:27 -0500

    Time: 288 ms (71.03%), Space: 103.7 MB (40.88%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -3,8 +3,8 @@ public:
     int minimumRounds(vector<int>& tasks) {
         unordered_map<int, int> freq;
         int result = 0;
-        for(int task: tasks) freq[task]++;        
-        for(auto each: freq) {
+        for(int& task: tasks) freq[task]++;        
+        for(auto& each: freq) {
             if(each.second == 1) return -1;
             result += (each.second+2)/3;
         }
