66759940628f1724839e292861bc7ac3ea74acf6
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-26 16:45:34 -0500

    Time: 4 ms (78.19%), Space: 5.9 MB (70.27%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
-        priority_queue<int> heap;
-        heap.push(a); heap.push(b); heap.push(c);
-        int count = 0;
-        while(heap.size() > 1) {
-            int one = heap.top(); heap.pop();
-            int two = heap.top(); heap.pop();
-            if(one > 1) heap.push(one-1);
-            if(two > 1) heap.push(two-1);
-            count++;
-        }
-        return count;
+        return min((a+b+c)/2, a+b+c-max(max(a, b), c));
     }
 };
