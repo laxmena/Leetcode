0391c7c0bd83e3aa30f88713feff8b895e35431f
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-25 21:49:46 -0500

    Time: 11 ms (98.96%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -18,8 +18,7 @@ public:
         TreeNode* right = lca(root->right, p, q);
         if(right != NULL && left != NULL) return root;
         if(right != NULL) return right;
-        if(left != NULL) return left;
-        return NULL;
+        return left;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         return lca(root, p, q);        
