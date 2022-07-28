f1fba334c54eb4d4fd2ad652bf859223627d032f
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-28 18:57:58 -0500

    Time: 776 ms (100.00%), Space: 60.2 MB (100.00%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -8,19 +8,24 @@ public:
         }
         return sum;
     }
+    
     int maximumSum(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-        
-        map<int, vector<int>> m;
+        map<int, pair<int, int>> m;
         for(auto num: nums) {
-            m[digitSum(num)].push_back(num);
+            int sum = digitSum(num);
+            cout << "Sum: " << sum << "\n";
+            if(m.find(sum) == m.end()) {
+                m[sum] = make_pair(INT_MIN, num); //max2, max1            
+            } else if(num >= m[sum].second) {
+                m[sum].first = m[sum].second;
+                m[sum].second = num;
+            } else if(num >= m[sum].first) {
+                m[sum].first = num;
+            }    
         }
         int maxSum = -1;
         for(auto kp: m) {
-            int n = kp.second.size();
-            if(n>1) {
-                maxSum = max(maxSum, kp.second[n-1] + kp.second[n-2]);
-            }
+            maxSum = max(maxSum, kp.second.second + kp.second.first);
         }
         return maxSum;
     }
