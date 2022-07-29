c4e53cf4bcb6ec52c515bb161aa57ad54144d90e
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-07-28 20:12:39 -0500

    Time: 0 ms (100.00%), Space: 6.6 MB (81.33%) - LeetHub

--- 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -8,34 +8,30 @@ public:
         return diff == 1;
     }
     
-    int recurse(string& gene, string& end, int level, vector<string>& bank, map<string, int> visited) {
+    int recurse(string& gene, string& end, int level, vector<string>& bank, set<string>& visited) {
         
         if(gene == end) {
-            // cout << "Gene = End" << gene << " -> " << level << "\n";
             return level;
         }
         
-        visited[gene] = level;
+        visited.insert(gene);
         int minVal = INT_MAX;
         
-        // cout << "Level " << level << " => " << gene << "\n";
-        
         for(string m: bank) {
-            if(visited.find(m) != visited.end()) continue;            
+            if(visited.count(m) > 0) continue;            
             if(isMutation(gene, m)) {
-                // cout << "Mutation:  " << gene << " -> " << m << "\n";
                 int val = recurse(m, end, level+1, bank, visited);
-                // cout << "val => " << val << "\n";
                 if(val != -1 && val < minVal) {
                     minVal = val;
                 }                
             }
         }
+        visited.erase(gene);
         return minVal == INT_MAX? -1: minVal;
     }
     
     int minMutation(string start, string end, vector<string>& bank) {
-        map<string, int> visited;
+        set<string> visited;
         int count = recurse(start, end, 0, bank, visited);
         return count;
     }
