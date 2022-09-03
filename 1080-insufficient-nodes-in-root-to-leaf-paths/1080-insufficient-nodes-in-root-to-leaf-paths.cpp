a57b4c38184c9c7b22246ec572b514c8cc9964e3
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-09-03 01:38:49 -0500

    Time: 113 ms (5.49%), Space: 33 MB (57.95%) - LeetHub

--- 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.cpp
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.cpp
@@ -24,11 +24,10 @@ public:
         if(node->left == NULL && right == false) return false;
         if(node->right == NULL && left == false) return false;
 
-        int result = left | right;
         if(!left) node->left = NULL;
         if(!right) node->right = NULL;        
         
-        return result;
+        return left | right;
     } 
     TreeNode* sufficientSubset(TreeNode* root, int limit) {
         limitVal = limit;
