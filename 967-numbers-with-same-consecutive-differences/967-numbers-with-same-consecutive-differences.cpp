7e54c770910698f79799eb9d356486c9b064cce0
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-09-03 00:18:26 -0500

    Time: 15 ms (9.53%), Space: 9.6 MB (11.96%) - LeetHub

---
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> numsSameConsecDiff(int n, int k) {
+        set<int> result;
+        for(int i=1; i<10; i++) result.insert(i);
+        
+        while(n > 1) {
+            set<int> temp;
+            for(auto val: result) {
+                int digit = val % 10;
+                if((digit - k) >= 0) {
+                    temp.insert(val*10 + (digit-k));
+                }
+                if((digit + k) < 10) {
+                    temp.insert(val*10 + (digit+k));
+                }
+            }
+            result = temp;
+            n -= 1;
+        }
+        
+        vector<int> resvec(begin(result), end(result));
+        return resvec;
+    }
+};
