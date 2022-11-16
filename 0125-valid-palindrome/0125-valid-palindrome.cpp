cf8da53068e6d55f4b362a773202ca712190b793
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-11-16 14:51:29 -0600

    Time: 13 ms (51.44%), Space: 7.4 MB (78.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,14 +1,16 @@
 class Solution {
-public:
-    bool isPalindrome(string s) {
-        string s1 = "";
-        for(char c: s) {
-            if(c >= 'a' && c <= 'z') s1 += c;
-            if(c >= 'A' && c <= 'Z') s1 += char(c+32);
-            if(c >= '0' && c <= '9') s1 += c;
-        }
-        for(int i=0; i<s1.size()/2; i++)
-            if(s1[i] != s1[s1.size()-i-1]) return false;
-        return true;
+ public:
+  bool isPalindrome(string s) {
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+      while (i < j && !isalnum(s[i]))
+        i++;
+      while (i < j && !isalnum(s[j]))
+        j--;
+
+      if (tolower(s[i]) != tolower(s[j]))
+        return false;
     }
+
+    return true;
+  }
 };
