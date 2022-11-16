ef20bdbbe211bd2e769d0920e58de2a198538c70
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-11-16 13:37:32 -0600

    Time: 4 ms (97.98%), Space: 7.2 MB (82.70%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s), end(s));
-        sort(begin(t), end(t));
-        return s == t;        
+        vector<int> hist(26, 0);
+        for(char c: s) hist[c-'a']++;
+        for(char c: t) hist[c-'a']--;
+        for(int i: hist) if(i != 0) return false;
+        return true;
     }
 };
