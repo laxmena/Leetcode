4f499166b8ea6a36c125bfe27341a583250b7b11
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-11-16 17:48:58 -0600

    Time: 58 ms (24.64%), Space: 18.1 MB (21.59%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int res = 0;
-        for(int n: nums) res = res ^ n;
-        for(int i=0; i<= nums.size(); i++) res = res ^ i;
+        for(int i=1; i<=nums.size(); i++) {
+            res = res ^ i;
+            res = res ^ nums[i-1];
+        }
         return res;
     }
 };
