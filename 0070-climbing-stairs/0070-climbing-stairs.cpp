5611096012796337ef23d51e64a9acc9d196f2fd
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2022-11-17 19:22:08 -0600

    Time: 4 ms (21.38%), Space: 6.3 MB (15.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     map<int, int> m;
     int climbStairs(int n) {
-        if(n <= 1) return 1;
+        if(n < 2) return 1;
         if(m.count(n) > 0) return m[n];
         m[n] = climbStairs(n-1) + climbStairs(n-2);
         return m[n];
